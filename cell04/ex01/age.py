72430d9ed0bcb128fa0800a32ff4f6ffcd74e0b1
fahsaisabai <fahsaisabai.s@gmail.com>
2025-06-22 13:25:17 +0000

    corrected cell04ex01

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+age = int(input("Please tell me your age: "))
+
+print("You are currently", age, "years old.")
+print("In 10 years, you'll be", age + 10, "years old.")
+print("In 10 years, you'll be", age + 20, "years old.")
+print("In 10 years, you'll be", age + 30, "years old.")
