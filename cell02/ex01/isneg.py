e8aee990ebdf2b7308a217117a7a09a8de8a3f95
fahsaisabai <fahsaisabai.s@gmail.com>
2025-06-22 07:55:26 +0000

    corrected cell02ex01

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-num = input("")
-if num < "0":
+num = int(input(""))
+if num < 0:
     print ("This number is negative.")
 
-if num > "0":
+if num > 0:
     print ("This number is positive.")
 
-if num == "0":
+if num == 0:
     print ("This number is both positive and negative.")
