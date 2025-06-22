718a003e08293dc1d89b91b80439a3a1e33d89d9
fahsaisabai <fahsaisabai.s@gmail.com>
2025-06-22 13:06:47 +0000

    corrected cell04ex04

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -5,7 +5,7 @@ second_number = int(input("Give me the second number: "))
 
 print("Thank you!")
 
-print(f"{first_number} + {second_number} = {first_number + second_number}")
-print(f"{first_number} - {second_number} = {first_number - second_number}")
-print(f"{first_number} / {second_number} = {first_number // second_number}")
-print(f"{first_number} * {second_number} = {first_number * second_number}")
+print(first_number, "+", second_number, "=", first_number + second_number)
+print(first_number, "-", second_number, "=", first_number - second_number)
+print(first_number, "/", second_number, "=", first_number / second_number)
+print(first_number, "*", second_number, "=", first_number * second_number)
--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 import math
-
 user_input = input("Give me a number: ")
 
 number = float(user_input)
-
 rounded_up = math.ceil(number)
-
 print(rounded_up)
